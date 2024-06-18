f675bc4ffc65087616a43110b5e1088bb3c7cd98
wierzba100 <filipwierzbinka@gmail.com>
2024-06-18 05:25:43 +0200

    jupyter notebook update

--- Sobel_filter_rtl/Sobel_filter.runs/synth_1/rundef.js
+++ Sobel_filter_rtl/Sobel_filter.runs/synth_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 ISEStep( "vivado",
-         "-log sobel_design_wrapper.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source sobel_design_wrapper.tcl" );
+         "-log top.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source top.tcl" );
 
 
 
